62e4dcfe7c4cb032ea5cfae7a2002997c881046e
Jorge Gabitto <jgabitto1792@gmail.com>
2020-08-13 21:00:56 -0500

    Changed googleOAuth link to heroku link

--- src/googleAuth/googleOAuth.js
+++ src/googleAuth/googleOAuth.js
@@ -1,12 +1,16 @@
 const { google } = require('googleapis');
 
+const LOCAL_LINK = "http://localhost:3000/loginGoogle";
+const LINK = "https://vegeloper-app.herokuapp.com/loginGoogle";
+
+
 const oauth2Client = new google.auth.OAuth2(
     // client id
     `${process.env.CLIENT_ID}`,
     // client secret
     `${process.env.CLIENT_SECRET}`,
     // link to redirect
-    "http://localhost:3000/loginGoogle"
+    LINK
 )
 
 module.exports = oauth2Client;
