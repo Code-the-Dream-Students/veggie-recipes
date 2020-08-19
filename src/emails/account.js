523cd1d63eb46f8d4db7b495d952d9eeb8691931
DeeTheDev <deethedev@gmail.com>
2020-08-18 20:12:04 -0400

    Configured mongoDB and setup app

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,4 +1,6 @@
-const sgMail = require('@sendgrid/mail');
+const dotenv = require("dotenv");
+dotenv.config();
+const sgMail = require("@sendgrid/mail");
 
 const sendgridAPIKEY = `${process.env.EMAIL_API_KEY}`;
 
@@ -7,47 +9,47 @@ sgMail.setApiKey(sendgridAPIKEY);
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'jgabitto1792@gmail.com',
-        subject: 'Thanks for joining Veggie Recipes!',
-        text: `Welcome to Veggie Recipes ${name}! Hope you enjoy our app!`
-    })
-}
+        from: "jgabitto1792@gmail.com",
+        subject: "Thanks for joining Veggie Recipes!",
+        text: `Welcome to Veggie Recipes ${name}! Hope you enjoy our app!`,
+    });
+};
 
 const sendForgotPasswordEmail = (email, name, password) => {
     sgMail.send({
         to: email,
-        from: 'jgabitto1792@gmail.com',
-        subject: 'Request for forgotten password',
+        from: "jgabitto1792@gmail.com",
+        subject: "Request for forgotten password",
         html: `Hello ${name}! Here is a temporary password to login. Once you're logged in, you can reset your password after you're logged in. 
                 <div>
                     <strong>Password:</strong> ${password}
-                </div>`
-    })
-}
+                </div>`,
+    });
+};
 
 const resetPasswordEmail = (email, name, password) => {
     sgMail.send({
         to: email,
-        from: 'jgabitto1792@gmail.com',
-        subject: 'Request for forgotten password',
+        from: "jgabitto1792@gmail.com",
+        subject: "Request for forgotten password",
         html: `Hello ${name}! Here is a temporary password to login. Once you're logged in, you can reset your password after you're logged in. 
                 <div>
                     <strong>Password:</strong> ${password}
-                </div>`
-    })
-}
+                </div>`,
+    });
+};
 
 const newPasswordEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'jgabitto1792@gmail.com',
-        subject: 'Confirmation of password change',
-        html: `Hello ${name}! You have succesfully changed your password.`
-    })
-}
+        from: "jgabitto1792@gmail.com",
+        subject: "Confirmation of password change",
+        html: `Hello ${name}! You have succesfully changed your password.`,
+    });
+};
 
 module.exports = {
     sendWelcomeEmail,
     resetPasswordEmail,
-    newPasswordEmail
+    newPasswordEmail,
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const app = require('./app');
+const dotenv = require("dotenv");
+dotenv.config();
+const app = require("./app");
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
-})
+});
