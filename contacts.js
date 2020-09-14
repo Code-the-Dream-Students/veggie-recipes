0f7639bae195d1d76b841bd3eda98c9035980259
andresochoa91 <andres_ochoa91@hotmail.com>
2020-09-14 10:54:51 -0700

    Contacts updated, still missing some data

---
+++ contacts.js
@@ -0,0 +1,58 @@
+const developers = [
+  {
+    name: "Johan Ochoa",
+    description: "Passionate, hard-working, easy going and curious programmer. Polyglot who speaks 4 languages (English, Spanish, Portuguese and Italian). Looking to use my coding skills to solve problems in daily life. Capable of being self-directed and learning new things. Goal-oriented sincere worker that dedicates himself fully to his projects. Always willing to teach what he knows and learn from others. Proficient in JavaScript, ReactJS, Typescript, Styled Components, HTML, CSS, ES6, Object Oriented and Functional Programming.",
+    picture: "./contacts-picture/johan.jpg",
+    linkedin: "https://www.linkedin.com/in/jandresochoa91/",
+    github: "https://github.com/andresochoa91"
+  },
+  {
+    name: "Jorge Gabitto",
+    description: "",
+    picture: "./contacts-picture/jorge.jpg",
+    linkedin: "",
+    github: ""
+  },
+  {
+    name: "Martina Pantelic",
+    description: "Creative Web Developer skilled in making highly optimized, well crafted responsive websites and apps with strong sense for UX and UI. Primarily focused on developing using React, HTML5, CSS3, JQuery, Javascript with designer background and proficiency in Adobe tools- mainly Photoshop and Illustrator. Always excited to explore the inspiring connection between engineering and design.",
+    picture: "./contacts-picture/martina.jpg",
+    linkedin: "https://www.linkedin.com/in/martina-pantelic/",
+    github: "https://github.com/MartinaPantelic"
+  },
+  {
+    name: "Lu Ar",
+    description: " I'm am studying to be a Front-End Dev, I also like UX cause I always want to create cool things. Born and raised in Mexico. I have worked as a web developer and UI Designer. In my spare time, I like to sew, watch sports and go out for a walk.",
+    picture: "",
+    linkedin: "",
+    github: "https://github.com/luar1",
+  },
+  {
+    name: "Natalia Cielinski",
+    description: "Bilingual and fast-learner professional with a diverse and international educational background, including Marketing and entry-level Front-End Web Developer. Possesses skills in digital marketing such as social media strategy, content creation, and advertising, along with a variety of web programming skills such as React.js, JavaScript, HTML, and CSS. Shows a passion for technology and an interest in forward-thinking projects and companies. Recognizes this is an ever-changing world and plans to stay one step ahead.",
+    picture: "./contacts-picture/natalia.png",
+    linkedin: "https://www.linkedin.com/in/natalia-cielinski-7a8031142/",
+    github: "https://github.com/natacski"
+  },
+  {
+    name: "Melissa Ferguson",
+    description: "",
+    picture: "./contacts-picture/melissa.jpg",
+    linkedin: "http://linkedin.com/in/melissa-ferguson-a2b74913",
+    github: "https://github.com/meljoi"
+  },
+  {
+    name: "Dana Seidakhmetova",
+    description: "Self-motivated and highly reliable student of Information Technology. Passionate about computer science, programming, and computer networks.",
+    picture: "./contacts-picture/dana.jpg",
+    linkedin: "http://linkedin.com/in/dana-seidakhmetova-b90842173",
+    github: "https://github.com/dseydahmetova"
+  },
+  {
+    name: "Dejan",
+    description: "",
+    picture: "",
+    linkedin: "",
+    github: ""
+  },
+];
