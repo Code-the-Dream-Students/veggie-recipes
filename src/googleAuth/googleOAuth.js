48534d821c38bc49adc9be6dd7d036bbd8ec19e8
Jorge Gabitto <jgabitto1792@gmail.com>
2020-09-20 13:14:53 -0500

    Fixed constants file and googleOAuth

--- src/googleAuth/googleOAuth.js
+++ src/googleAuth/googleOAuth.js
@@ -1,17 +1,13 @@
 const { google } = require('googleapis');
-
-const LOCAL_LINK = "http://localhost:3000/loginGoogle";
-const LINK = "https://vegeloper.herokuapp.com/loginGoogle";
-
-
+const link = require('../../config/constants');
 
 const oauth2Client = new google.auth.OAuth2(
     // client id
-    `${process.env.CLIENT_ID}`,
+    process.env.CLIENT_ID,
     // client secret
-    `${process.env.CLIENT_SECRET}`,
+    process.env.CLIENT_SECRET,
     // link to redirect
-    LINK
+    link.LINK
 )
 
 module.exports = oauth2Client;
