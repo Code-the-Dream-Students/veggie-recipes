045799cfcd33e2158cff397a6c168f69b8f8deaf
Denilson V <deethedev@gmail.com>
2020-09-23 22:29:13 -0400

    Client-side and server-side register form validation update

--- public/js/form-validation.js
+++ public/js/form-validation.js
@@ -1,79 +1,81 @@
 //----------------------------- CLIENT-SIDE REGISTER INPUT VALIDATION -------------- DeeTheDev
-// ** Boostrap example validation
-window.addEventListener(
-    "load",
-    function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName("needs-validation");
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener(
-                "submit",
-                function (event) {
-                    if (form.checkValidity() === false) {
+//Jquery doc on ready
+$(document).ready(()=> {
+    window.addEventListener(  "load",  () => {
+        registerForm.addEventListener("submit", (event)=> {
+            // ** Boostrap validation with checkValidity
+            if (registerForm.checkValidity() === false) {
                         event.preventDefault();
                         event.stopPropagation();
                     }else{
                         event.preventDefault();
-                        retriveData();
+                        // Register User with Ajax call OR return error msg
+                        sendRegisterPostAjax();
+                        event.stopPropagation();
                     }
-                    form.classList.add("was-validated");
-                },
-                false
-            );
+                    registerForm.classList.add("was-validated");
+                })
+        },
+        false
+    );
+    
+    //Server-side error handling.
+    //Helpful resource: https://gist.github.com/chrislkeller/3230081
+
+    const jqueryNoConflict = jQuery;
+    
+    //send data along with POST request to REGISTER route
+    sendRegisterPostAjax = () => {
+        const registerForm = document.getElementById("registerForm");
+        const registerFormData = new FormData(registerForm);
+        const data = {
+            userName: registerFormData.get("userName"),
+            email: registerFormData.get("email"),
+            password: registerFormData.get("password"),
+            confirmPassword: registerFormData.get("confirmPassword"),
+        };
+        let source;
+        jqueryNoConflict.ajax({
+            type: "POST",
+            url: "/register",
+            data,
+        })
+        .done(() => {
+            //when user created > redirect home as USER loggedIn
+            window.location.href = window.location.origin + '/';
+        })
+        .error((errors) => {
+            //JSON.parse loads data into object
+            source = JSON.parse(errors.responseText);
+            // Handlebars DEBUGGER
+            // handlebarsDebugHelper();
+            //Load JSON and renderDataVisualsTemplate function
+            renderHandlebarsTemplate('/errors/errorHandling.hbs', '#errorResponse', source);
+        })
+    }
+    function getTemplateWithErrors(path, callback) {
+        let source, template;
+        jqueryNoConflict.ajax({
+            url: path,
+            success: (data) => {
+                source = data;
+                template = Handlebars.compile(source);
+                if(callback) callback(template);
+            }
+        })
+    }
+    function renderHandlebarsTemplate(withTemplatePath, inElement, withData){
+        getTemplateWithErrors(withTemplatePath, function(template) {
+            jqueryNoConflict(inElement).html(template(withData));
+        })
+    }
+    // add handlebars debugger
+    handlebarsDebugHelper = () =>{
+        Handlebars.registerHelper("debug", (optionalValue) => {
+            console.log("Current Context");
+            console.log("====================");
+            console.log(this);
         });
-    },
-    false
-);
+    };
 
-//Server-side error handling.
-// source from: https://gist.github.com/chrislkeller/3230081
-
-var jqueryNoConflict = jQuery;
-
-// jqueryNoConflict("#submitRegisterForm").(function(){
-//     retriveData();
-// });
-
-// grab data
-function retriveData() {
-    var dataSource = '/errorHandling';
-    jqueryNoConflict.getJSON(dataSource, renderDataVisualsTemplate);
-};
-
-// render compiled handlebars template
-function renderDataVisualsTemplate(data){
-    const errorHandlingHbs = $('#the-template').html;
-    handlebarsDebugHelper();
-    renderHandlebarsTemplate(errorHandlingHbs, '#errorResponse', data);
-};
-
-// render handlebars templates via ajax
-function getTemplateAjax(path, callback) {
-    var source, template;
-    jqueryNoConflict.ajax({
-        url: path,
-        success: function (data) {
-            source = data;
-            template = Handlebars.compile(source);
-            console.log(source);
-            if (callback) callback(template);
-        }
-    });
-};
-
-// function to compile handlebars template
-function renderHandlebarsTemplate(withTemplate,inElement,withData){
-    getTemplateAjax(withTemplate, function(template) {
-        jqueryNoConflict(inElement).html(template(withData));
-    })
-};
-
-// add handlebars debugger
-function handlebarsDebugHelper(){
-    Handlebars.registerHelper("debug", function(optionalValue) {
-        console.log("Current Context");
-        console.log("====================");
-        console.log(this);
-    });
-};
+})
