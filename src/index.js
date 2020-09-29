3924183effcd9e2f185cb37a6df463a5f1a31962
Denilson V <deethedev@gmail.com>
2020-09-29 19:23:20 -0400

    Fixed app.listen - removed localhost host

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const app = require("./app");
 
 // App Listener setup
 const port = process.env.PORT;
-const hostname = "localhost" || "127.0.0.1";
 
-app.listen(port, hostname, () => {
-    console.log(`Server is up on port ${port} : http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
